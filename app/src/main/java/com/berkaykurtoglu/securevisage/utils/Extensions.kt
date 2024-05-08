302c634c5ec2fedd82c79ed8347bf777a481c2f9
Berkay <berkayberkay300@gmail.com>
2024-05-08 23:31:48 +0300

    Uri to bitmap extension func implemented

--- app/src/main/java/com/berkaykurtoglu/securevisage/utils/Extensions.kt
+++ app/src/main/java/com/berkaykurtoglu/securevisage/utils/Extensions.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.net.Uri
 import android.provider.MediaStore
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import java.io.ByteArrayOutputStream
 
 
@@ -18,6 +20,10 @@ fun Bitmap.bitmapToUri(
     val path = MediaStore.Images.Media.insertImage(context.contentResolver, this, "Title", null)
     return Uri.parse(path)
 
+}
+
+fun Uri.uriToBitmap(context : Context) : ImageBitmap {
+    return MediaStore.Images.Media.getBitmap(context.contentResolver, this).asImageBitmap()
 }
 
 
