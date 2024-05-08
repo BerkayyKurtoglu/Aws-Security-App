2d068f476effffc7054b20c239e3c462e5583f85
Berkay <berkayberkay300@gmail.com>
2024-05-08 22:36:58 +0300

    HomeOwner event and state updated

--- app/src/main/java/com/berkaykurtoglu/securevisage/presentation/HomeOwners/HomeOwnerEvent.kt
+++ app/src/main/java/com/berkaykurtoglu/securevisage/presentation/HomeOwners/HomeOwnerEvent.kt
@@ -6,4 +6,6 @@ sealed class HomeOwnerEvent {
 
     data object OnFirstTimeCall : HomeOwnerEvent()
 
+    data class OnGetHomeOwnerPic(val key : String) : HomeOwnerEvent()
+
 }
--- app/src/main/java/com/berkaykurtoglu/securevisage/presentation/HomeOwners/HomeOwnerState.kt
+++ app/src/main/java/com/berkaykurtoglu/securevisage/presentation/HomeOwners/HomeOwnerState.kt
@@ -1,9 +1,12 @@
 package com.berkaykurtoglu.securevisage.presentation.HomeOwners
 
+import java.net.URL
+
 data class HomeOwnerState(
 
     val userList: List<String> = mutableListOf(),
     val isLoading: Boolean = false,
-    val errorMessage: String = ""
+    val errorMessage: String = "",
+    val selectedUserImage : URL? = null
 
 )
