a0df755dff38dc576586c1272411023583b17f6d
Berkay <berkayberkay300@gmail.com>
2024-05-08 18:43:18 +0300

    Error UI is created for HomeOwner Screen

---
+++ app/src/main/java/com/berkaykurtoglu/securevisage/presentation/HomeOwners/ErrorScreen.kt
@@ -0,0 +1,41 @@
+package com.berkaykurtoglu.securevisage.presentation.HomeOwners
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun ErrorScreen(
+    modifier: Modifier = Modifier,
+    message: String,
+    onRetry: () -> Unit
+) {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = message
+        )
+        FilledTonalButton(
+            onClick = onRetry
+        ) {
+            Text(text = "Tekrar Dene")
+        }
+    }
+
+}
+
+@Preview
+@Composable
+private fun ErrorScreenPrev() {
+    ErrorScreen(message = "Test"){}
+}
+
+
+
