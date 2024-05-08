075bc480161a287bd0ec00d2d7336abe693d7f79
Berkay <berkayberkay300@gmail.com>
2024-05-08 23:44:53 +0300

    EntryScreen's user image is converted from async image to image compasable

--- app/src/main/java/com/berkaykurtoglu/securevisage/presentation/EntryScreen/EntryScreen.kt
+++ app/src/main/java/com/berkaykurtoglu/securevisage/presentation/EntryScreen/EntryScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -50,6 +51,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
@@ -186,6 +188,8 @@ fun EntryScreen(
                                 .clickable {
                                     showBottomSheet.value = true
                                 }
+                                .size(250.dp),
+                            contentScale = ContentScale.Crop
                         )
                         Spacer(modifier = Modifier.height(20.dp))
                         Row(
