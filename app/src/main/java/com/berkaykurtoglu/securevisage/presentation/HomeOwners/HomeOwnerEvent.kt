4e339264fb374397cae1b65090ef92c8956b2668
Berkay <berkayberkay300@gmail.com>
2024-05-14 01:26:17 +0300

    HomeOwnerEvent OnUploadNewUser event is implemented

--- app/src/main/java/com/berkaykurtoglu/securevisage/presentation/HomeOwners/HomeOwnerEvent.kt
+++ app/src/main/java/com/berkaykurtoglu/securevisage/presentation/HomeOwners/HomeOwnerEvent.kt
@@ -1,5 +1,7 @@
 package com.berkaykurtoglu.securevisage.presentation.HomeOwners
 
+import android.net.Uri
+
 sealed class HomeOwnerEvent {
 
     data object OnRetryEvent : HomeOwnerEvent()
@@ -7,5 +9,11 @@ sealed class HomeOwnerEvent {
     data object OnFirstTimeCall : HomeOwnerEvent()
 
     data class OnGetHomeOwnerPic(val key : String) : HomeOwnerEvent()
+
+    data class OnUploadNewUser(
+        val name : String,
+        val uri : Uri
+    ) : HomeOwnerEvent()
+
 
 }
